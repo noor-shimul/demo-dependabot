27485bb932bca46874ca693e16fb76f8bf4a138d
noor-shimul <103429787+noor-shimul@users.noreply.github.com>
2025-09-29 20:43:56 +0100

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "=1.9.8"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+     version = "4.38.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+}
