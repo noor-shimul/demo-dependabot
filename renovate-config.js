a2df4c41e9de722c0d4279286808e1362d49ac59
noor-shimul <103429787+noor-shimul@users.noreply.github.com>
2025-11-11 16:49:55 +0000

    Create renovate-config.js

---
+++ renovate-config.js
@@ -0,0 +1,25 @@
+{
+  "extends": ["config:recommended"],
+  "enabledManagers": ["terraform"],
+  "dependencyDashboard": true,
+  "packageRules": [
+    {
+      "matchManagers": ["terraform"],
+      "matchDepNames": ["azurerm", "azuread"],
+      "groupName": "Azure Terraform Providers",
+      "groupSlug": "azure-providers",
+      "matchDatasources": ["terraform-provider"],
+      "matchFilePatterns": ["**/*.tf"]
+    },
+    {
+      "matchManagers": ["terraform"],
+      "matchDatasources": ["terraform-provider", "terraform-module"],
+      "additionalBranchPrefix": "{{baseDir}}-",
+      "matchFilePatterns": ["**/terraform/**"]
+    }
+  ],
+  "terraform": {
+    "enabled": true,
+    "fileMatch": ["**/*.tf"]
+  }
+}
